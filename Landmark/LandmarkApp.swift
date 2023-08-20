6098d1535ced92565c626a33ae7a1aaa758396bc
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-08-20 18:00:56 +0530

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Pranav Choudhary on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
