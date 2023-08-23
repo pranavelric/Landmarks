66ea37b608680246ae623244ac6d24613f4f3c62
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-08-23 13:44:16 +0530

    Added Hike view animations

--- Landmark/Views/Hikes/HikeGraph.swift
+++ Landmark/Views/Hikes/HikeGraph.swift
@@ -7,6 +7,15 @@ The elevation, heart rate, and pace of a hike plotted on a graph.
 
 import SwiftUI
 
+extension Animation {
+    static func ripple(index: Int) -> Animation {
+        Animation.spring(dampingFraction: 0.5).speed(2)
+            .delay(0.03 * Double(index))
+    }
+}
+
+
+
 struct HikeGraph: View {
     var hike: Hike
     var path: KeyPath<Hike.Observation, Range<Double>>
@@ -39,7 +48,7 @@ struct HikeGraph: View {
                         height: proxy.size.height,
                         range: observation[keyPath: path],
                         overallRange: overallRange
-                    )
+                    ) .animation(.ripple(index: index))
                 }
                 .offset(x: 0, y: proxy.size.height * heightRatio)
             }
