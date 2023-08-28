939f636540486451101dd888d554fd1edab2ae8e
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-08-28 23:31:49 +0530

    Added custom page control

---
+++ Landmark/Views/PageView/PageControl.swift
@@ -0,0 +1,20 @@
+//
+//  PageControl.swift
+//  Landmark
+//
+//  Created by Pranav Choudhary on 28/08/23.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageControl_Previews: PreviewProvider {
+    static var previews: some View {
+        PageControl()
+    }
+}
