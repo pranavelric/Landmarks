a2df1513df890083daa9fe09ff74f20404a3f9dc
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-08-29 16:27:11 +0530

    Landmark list added

--- Landmarks Watch App/ContentView.swift
+++ Landmarks Watch App/ContentView.swift
@@ -9,18 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        LandmarkList().environmentObject(ModelData())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(ModelData())
     }
 }
