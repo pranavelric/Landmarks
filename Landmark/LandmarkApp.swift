4c2c5911775413a98799f9808448bde585e1a107
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-08-29 19:41:56 +0530

    Added custom favorite command

--- Landmark/LandmarkApp.swift
+++ Landmark/LandmarkApp.swift
@@ -14,6 +14,11 @@ struct LandmarkApp: App {
         WindowGroup {
             ContentView().environmentObject(modelData)
         }
+        #if !os(watchOS)
+        .commands(content: {
+            LandmarkCommands()
+        })
+        #endif
         
        #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
